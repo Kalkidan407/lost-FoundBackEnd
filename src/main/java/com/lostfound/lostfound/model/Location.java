beb4639c480cae3a7e73e8242f90a8f72f295d52
Kalkidan Kelemework <kelemework267@gmail.com>
2025-11-17 22:21:38 +0000

    swagger added

--- src/main/java/com/lostfound/lostfound/model/Location.java
+++ src/main/java/com/lostfound/lostfound/model/Location.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@Table(name = "location")
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/lostfound/lostfound/model/Report.java
+++ src/main/java/com/lostfound/lostfound/model/Report.java
@@ -5,8 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
+@Table(name = "report")
 public class Report {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
