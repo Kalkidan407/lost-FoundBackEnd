49566a8eb1012486513c434b29988c37765d1969
Kalkidan Kelemework <kelemework267@gmail.com>
2026-02-14 20:34:07 +0000

    refactore:make User in repo ootional for safe handling during when user null

--- src/main/java/com/lostfound/lostfound/repository/UserRepository.java
+++ src/main/java/com/lostfound/lostfound/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.lostfound.lostfound.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +9,7 @@ import com.lostfound.lostfound.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
     
 }
