fa845dd6a4cdc8a8315ab75a3c83ff10f4c5e10e
Kalkidan Kelemework <kelemework267@gmail.com>
2026-03-04 08:17:32 +0000

    fix: fix 403 error

--- src/main/java/com/lostfound/lostfound/config/SecurityConfig.java
+++ src/main/java/com/lostfound/lostfound/config/SecurityConfig.java
@@ -4,14 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.lostfound.lostfound.security.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    
+
+    private final JwtAuthenticationFilter jwtFilter;
+
      @Bean
      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         
@@ -33,7 +43,9 @@ public class SecurityConfig {
         "/swagger-ui.html"
                 ).permitAll()
                 .anyRequest().authenticated()
-            );
+            )
+             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); 
+            ;
 
             return http.build();
      }
