30c2cb331bb6da7f98b168ab9ba8c42e6ab5f958
Kalkidan Kelemework <kelemework267@gmail.com>
2026-03-20 05:31:05 +0000

    feat: add status enum

--- src/main/java/com/lostfound/lostfound/model/Item.java
+++ src/main/java/com/lostfound/lostfound/model/Item.java
@@ -15,6 +15,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -59,8 +61,11 @@ private User user;
      @Column(nullable = false, unique = true)
     private String photoUrl;
 
+     @Enumerated(EnumType.STRING) 
      @Column(nullable = false, unique = true)
     private boolean status;
+
+ 
 
   
 }
