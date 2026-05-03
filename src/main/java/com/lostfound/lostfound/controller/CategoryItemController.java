57946669d9e725212ef611b224ccbd55a4f614f0
Kalkidan Kelemework <kelemework267@gmail.com>
2026-05-03 13:24:22 +0100

    still fixing

--- src/main/java/com/lostfound/lostfound/controller/CategoryItemController.java
+++ src/main/java/com/lostfound/lostfound/controller/CategoryItemController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lostfound.lostfound.dto.category.CategoryRequest;
 import com.lostfound.lostfound.dto.category.CategoryResponse;
-import com.lostfound.lostfound.model.Category;
+
 import com.lostfound.lostfound.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
