f462ea3b54b509c5e166399817ae2a801612e0c7
Kalkidan Kelemework <kelemework267@gmail.com>
2026-05-19 00:23:00 +0100

    fiix:

--- src/main/java/com/lostfound/lostfound/service/ItemService.java
+++ src/main/java/com/lostfound/lostfound/service/ItemService.java
@@ -156,13 +156,13 @@ private ItemResponse toDTO(Item item) {
 
     User currentUser = getCurrentUser();
 
-    boolean isAdmin = currentUser.getRole().name().equals("ADMIN");
+    // boolean isAdmin = currentUser.getRole().name().equals("ADMIN");
 
-    boolean isOwner = item.getUser().getId().equals(currentUser.getId());
+    // boolean isOwner = item.getUser().getId().equals(currentUser.getId());
 
-    if(!isAdmin && !isOwner){
-        throw new RuntimeException("You are not allowed to delete this item");
-    }
+    // if(!isAdmin && !isOwner){
+    //     throw new RuntimeException("You are not allowed to delete this item");
+    // }
 
     item.setDeletedAt(LocalDateTime.now());
 
