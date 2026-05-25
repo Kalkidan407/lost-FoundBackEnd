7fbeffed2bc184fac86ac5f20d7707c5385af2d5
Kalkidan407 <kelemework267@gmail.com>
2026-05-25 12:09:14 -0400

    fix: still trying to fix version conflict

--- src/main/java/com/lostfound/lostfound/config/SecurityConfig.java
+++ src/main/java/com/lostfound/lostfound/config/SecurityConfig.java
@@ -38,9 +38,9 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/api/auth/**",
                     "/swagger-ui/**",
+                    "/swagger-ui/index.html",
                     "/v3/api-docs/**",
-                    "/v3/api-docs",
-                    "/swagger-ui.html"
+                    "/v3/api-docs"
                 ).permitAll()     
                  .anyRequest().authenticated()   
                 
