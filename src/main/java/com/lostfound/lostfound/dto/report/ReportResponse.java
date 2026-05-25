524a89ed177218bd08fe26232010fa03d5070db5
Kalkidan407 <kelemework267@gmail.com>
2026-05-25 02:58:22 -0400

    fix:remove the circular fields

--- src/main/java/com/lostfound/lostfound/dto/report/ReportResponse.java
+++ src/main/java/com/lostfound/lostfound/dto/report/ReportResponse.java
@@ -1,24 +1,16 @@
 package com.lostfound.lostfound.dto.report;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lostfound.lostfound.dto.item.ItemResponse;
-import com.lostfound.lostfound.dto.user.UserResponse;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.*;
 
 @Data
-
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class ReportResponse {
     
-    private Long id ;
-
-    private String  message;
+    private Long id;
+    private String message;
     private ItemResponse reportedItem;
-    @JsonIgnore
-    @Schema(hidden = true)
-    private UserResponse reportedBy;
 }
--- src/main/java/com/lostfound/lostfound/dto/user/UserResponse.java
+++ src/main/java/com/lostfound/lostfound/dto/user/UserResponse.java
@@ -1,18 +1,10 @@
 package com.lostfound.lostfound.dto.user;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.lostfound.lostfound.dto.item.ItemResponse;
-import com.lostfound.lostfound.dto.report.ReportResponse;
 import com.lostfound.lostfound.model.Role;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
@@ -24,11 +16,5 @@ public class UserResponse {
     private String name;
     private Role role;
     private String email;
-    @JsonIgnore
-    @Schema(hidden = true)
-    private List<ReportResponse> report;
-    @JsonIgnore
-    @Schema(hidden = true)
-    private List<ItemResponse> items;
     
 }
