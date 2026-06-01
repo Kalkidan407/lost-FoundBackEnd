39515bfe9bc3b9ea1742a00e0e3adeafda9a9295
Kalkidan407 <kelemework267@gmail.com>
2026-06-01 05:40:29 -0400

    fix: fix postgress exception

--- src/main/java/com/lostfound/lostfound/model/Location.java
+++ src/main/java/com/lostfound/lostfound/model/Location.java
@@ -2,6 +2,7 @@ package com.lostfound.lostfound.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import com.lostfound.lostfound.model.Item;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
